fe0b5bde976f04e48aa090c2c97160e2a9c63446
kiruhs <87802917+kiruhs@users.noreply.github.com>
2026-06-17 12:40:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,111 @@
+# type integer - immutable
+# x = 8
+#
+# x = 5
+# y = x
+# print(y)
+# print(id(x))
+# print(id(y))
+# x = 0
+# print(type(x))
+# print(x.__sizeof__())
+
+# print(id(x))
+# print(id(y))
+
+
+
+
+# type string - immutable, indexable, iterable
+
+# x = "Alexander "
+# y = x
+# print(type(x))
+# print(x[0])
+# x = "B"
+# print(x)
+# print(y)
+
+# v = 3.89
+# z = z + v
+# print(type(z))
+# z = float(z)
+# print(z)
+# z = z * 5
+# print(z)
+# x = y * 5 * 2
+# print(x)
+
+# z = 6
+# z /= 5
+# print(z)
+# v = 9
+# y = v // 5
+# x = v % 5
+# print(y)
+# print(x)
+
+# x = -4
+#
+# print(x%3)
+# y=z="0"
+# x = 8  # 1000
+# print(x*3)
+# print(dir())
+# print(dir(y))
+# print(x.bit_count())
+# z = 656347
+# print(len(z))
+# print(x[4])
+# print(len(x))
+# print(x[4:9:2])
+# print(x[5:])
+# print(x[9:3:-1])
+
+# type boolean
+# a = False
+# b = True
+# print(int(a))
+x = "Hello,GroUp 2051"
+z = "Hello,Group 2051"
+# print(id(x))
+# print(id(z))
+# c = 5
+# print(c == "5")
+# print(x == z)
+# print(x is z)
+
+# up = x.upper() # all letters in upper case
+# # print(up == z)
+# low = x.lower() # all letters in lower case
+# print(low)
+# cap = x.capitalize() # string starts with Upper
+# print(cap)
+# t = x.title() # each word starts with Upper
+# print(t)
+
+# print(x.replace("Hello", "Good bye"))
+
+# name = input("Enter the name: ")
+# age = int(input("Enter your age: "))
+# print(type(age))
+# # print("Hello,", name, sep="$%^%")
+# print("Hello", name, "your age is", age, ". Next year you will be", age + 1 )
+# # formatted string
+# print(f"Hello, {name}, your age is {age}. Next year you will be {age+1}.")
+
+# d = 2 / 3
+# g = 6/7
+# print(f"{d:.4f}")
+# print(g)
+
+# price = int(input("Enter the price: "))
+
+# if price > 50:
+#     print("It is so expensive")
+# elif price > 40:
+#     print("The price is OK")
+# else:
+#     print("The price is very low")
+
+# print("It is so expensive") if price > 50 else print("The price is OK") if price > 40 else print("The price is very low")
